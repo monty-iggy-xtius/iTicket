e5d66e08d0a53fd2dea318d2dac631568fe9eee3
ignatius monty-xtius <monmihalo2324@gmail.com>
2025-01-30 03:56:44 +0300

    Update settings.py file

--- iticket/settings.py
+++ iticket/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/userticketing'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "static", "userticketing")
+    # os.path.join(BASE_DIR, "static", "userticketing")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
