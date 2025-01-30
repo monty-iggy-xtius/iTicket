6a008972450832a9a3d13249b039483220d526e2
ignatius monty-xtius <monmihalo2324@gmail.com>
2025-01-30 03:26:32 +0300

    Update settings.py file

--- iticket/settings.py
+++ iticket/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "static", "userticketing")
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
